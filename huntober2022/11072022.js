95fbd9e0509c155d48fb8f9dd412baae6c4aea18
intelagense <intelagense@gmail.com>
2022-11-07 17:38:48 -0600

    MISSINGNUM.

---
+++ huntober2022/11072022.js
@@ -0,0 +1,28 @@
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the array and no difference with it, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+//arrays of numbers
+//return number
+
+
+function findDeletedNumber(arr1, arr2){
+    if(arr1.length === arr2.length) return 0
+    for (i of arr1){
+        if (i != arr2[arr2.indexOf(i)]){
+            return i
+        }
+    }
+}
+
+
+ findDeletedNumber([1,2,3,4,5], [3,4,1,5]) //, 2, 'Deletion')
+ findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]) //, 5, 'Deletion')
+ findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]) //, 0, 'No deletion')
