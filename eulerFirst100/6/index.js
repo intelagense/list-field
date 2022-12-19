01f9eea826e13bc2d835cf99f5dc3acd700906d9
Eric Wynn Romere <intelagense@gmail.com>
2022-12-19 14:47:21 -0600

    Complete Euler 6

---
+++ eulerFirst100/6/index.js
@@ -0,0 +1,16 @@
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+function sumSquared(total){
+  let runningSum = 0;
+  let runningSquare = 0
+  for (i = 1; i <= total; i++){
+    runningSum += i ** 2;
+    runningSquare += i
+  }
+  
+  console.log(Math.abs(runningSum - runningSquare ** 2))
+  
+  return
+}
+
+sumSquared(100)
