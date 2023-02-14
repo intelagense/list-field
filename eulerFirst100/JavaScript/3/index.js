c929022a6125a14de05fbec62418f471098c2d05
intelagense <intelagense@gmail.com>
2023-02-14 05:56:51 -0600

    adds additional optimization(debatable) for no reason

--- eulerFirst100/JavaScript/3/index.js
+++ eulerFirst100/JavaScript/3/index.js
@@ -74,6 +74,10 @@ function largestPrimeFactor(value) {
 }
 
 function isPrime(num) {
+  let lastDigit = +num.toString().slice(-1);
+  if (num > 2 && lastDigit % 2 === 0) {
+    return false;
+  }
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
       return false;
